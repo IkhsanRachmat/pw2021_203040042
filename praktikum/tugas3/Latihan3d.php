b38702571bdcce17e5362689cb85f6f363d80563
IkhsanRachmat <23ikhsanrachmat@gmail.com>
2021-03-18 14:42:24 +0700

    menyelesaikan tugas praktikum pertemuan 3

---
+++ praktikum/tugas3/Latihan3d.php
@@ -0,0 +1,113 @@
+<?php
+/*
+Nama    : Ikhsan Rachmat Alghifari
+Npm/Nrp : 203040042
+Shift   : 10.00 - 11.00 Jum'at
+https://github.com/IkhsanRachmat/pw2021_203040042
+*/
+?>
+<?php 
+    $pemainBola = [
+        [
+            "nama" => "Cristiano Ronaldo",
+            "klub" => "Juventus",
+            "main" => "100",
+            "gol" => "76",
+            "assist" => "30"
+        ],
+        [
+            "nama" => "Lionel Messi",
+            "klub" => "Baecelona",
+            "main" => "120",
+            "gol" => "80",
+            "assist" => "12"
+        ],
+        [
+            "nama" => "Luca Modric",
+            "klub" => "Real Madrid",
+            "main" => "87",
+            "gol" => "12",
+            "assist" => "48"
+        ],
+        [
+            "nama" => "Mohammad Salah",
+            "klub" => "Liverpool",
+            "main" => "90",
+            "gol" => "103",
+            "assist" => "8"
+        ],
+        [
+            "nama" => "Neymar Jr",
+            "klub" => "Paris Saint Germain",
+            "main" => "109",
+            "gol" => "56",
+            "assist" => "15"
+        ],
+        [
+            "nama" => "Sadio Mane",
+            "klub" => "Liverpool",
+            "main" => "63",
+            "gol" => "30",
+            "assist" => "70"
+        ],
+        [
+            "nama" => "Zlatan Ibrahimovic",
+            "klub" => "Ac Milan",
+            "main" => "100",
+            "gol" => "10",
+            "assist" => "12"
+        ]
+    ];
+$jm_main = 0;
+$jm_gol= 0;
+$jm_assist = 0;
+foreach ($pemainBola as $pemain => $bola) {
+    $jm_main += $bola['main'];
+    $jm_gol += $bola['gol'];
+    $jm_assist += $bola['assist'];
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Latihan3d</title>
+    <style>
+        .kotak {
+            font-family: arial;
+            width: 40%;
+            padding: 10px;
+        }
+    </style>
+</head>
+<body>
+    <div class="kotak">
+        <table border="1" cellspacing="1" cellpadding="10">
+            <td><b>NO</b></td>
+            <td><b>NAMA</b></td>
+            <td><b>CLUB</b></td>
+            <td><b>MAIN</b></td>
+            <td><b>GOAL</b></td>
+            <td><b>ASSIST</b></td>
+            <tr></tr>
+            <?php  $i = 1; ?>    
+            <?php foreach ($pemainBola as $pb) : ?>     
+                    <td><?= $i; ?></td>
+                    <td><?= $pb["nama"];?></td>
+                    <td><?= $pb["klub"];?></td>
+                    <td><?= $pb["main"];?></td>
+                    <td><?= $pb["gol"];?></td>
+                    <td><?= $pb["assist"];?></td>
+                    <tr></tr>  
+                    <?php $i++; ?>
+                <?php endforeach; ?>
+                <th>#</th>
+                <th colspan="2">
+                <center>Jumlah</center>
+                </th>
+                <th><?= $jm_main ?></th>
+                <th><?= $jm_gol ?></th>
+                <th><?= $jm_assist ?></th>    
+        </table>
+    </div>
+</body>
+</html>
